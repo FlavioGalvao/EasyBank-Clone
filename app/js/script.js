286b9c65cd4a3eaeb3c070167a745c66ff7d1253
Flávio Galvão <flaviogalvao0912@gmail.com>
2021-01-19 14:07:13 -0300

    Adicionado o efeito do menu hamburguer

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,28 @@
-console.log("hello wolrd");
+const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const fadeElements = document.querySelectorAll('.has-fade');
+
+btnHamburger.addEventListener('click', function(){
+  console.log('click hamburger');// TESTE
+
+  if(header.classList.contains('open')){ // Fecha efeito Hamburger
+    body.classList.remove('noscroll');// remover as classes
+    header.classList.remove('open');    
+    fadeElems.forEach(function(element){
+      element.classList.remove('fade-in');
+      element.classList.add('fade-out');
+    });
+    
+  }
+  else { // Abre o menu Hamburger
+    body.classList.add('noscroll');//adiciona classes
+    header.classList.add('open');
+    fadeElements.forEach(function(element){
+      element.classList.remove('fade-out');
+      element.classList.add('fade-in');
+    });
+
+  }  
+});
